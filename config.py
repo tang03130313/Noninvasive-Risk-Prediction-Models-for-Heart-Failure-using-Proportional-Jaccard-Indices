d12d7e9965d3e0f355ee2129c9ce3c14e79ac81c
Yueh Tang <49488886+tang03130313@users.noreply.github.com>
2022-06-03 15:54:47 +0800

    version 1 config

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 
 import yaml
 import sys
-with open("./config/config_188.yaml", "r") as stream: yaml_data = yaml.safe_load(stream)  # 335
+with open("./config/config_sample222.yaml", "r") as stream: yaml_data = yaml.safe_load(stream)  # 335
 configname = yaml_data["inputs"]["configname"]
 category = yaml_data["inputs"]["category"]
 diseasename = yaml_data["inputs"]["diseasename"]
@@ -9,7 +9,7 @@ diseaseicd = yaml_data["inputs"]["diseaseicd"]
 diseasejson = yaml_data["inputs"]["diseasejson"]
 noramaljson = yaml_data["inputs"]["noramaljson"]
 ORthrehold = yaml_data["inputs"]["ORthrehold"]
-sup_filter = yaml_data["inputs"]["sup_filter"]
+supthreshold = yaml_data["inputs"]["supthreshold"]
 date_interval_flag = yaml_data["inputs"]["date_interval_flag"]
 health_endyear = yaml_data["inputs"]["health_endyear"]
 hospitialized = yaml_data["inputs"]["hospitialized"]
